d8e0f0827ec4a25b24bb01b6752647b190805e4c
EL93019205 <el93019205_2@yahoo.co.jp>
2021-07-31 05:19:03 +0900

    hello worldを変更し、ルートルーティングを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   def hello
-    render html:"hello,world!"
+    render html:"¡hola,mund!"
+  end
+  def goodbye
+    render html:"goodbye, world!"
   end
 end
